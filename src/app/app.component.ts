cd12e5b3ac31d873572aa48a621a6bee69e9ab71
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-05-26 15:49:46 -0500

    FormBuilder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Usuario } from './usuario';
 
 @Component({
@@ -10,16 +10,15 @@ import { Usuario } from './usuario';
 export class AppComponent implements OnInit{
   user:FormGroup;
 
-  constructor(){
-    this.user = new FormGroup({});
+  constructor(private fb:FormBuilder){
   }
   
   ngOnInit(){
-    this.user = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    account: new FormGroup({
-      email: new FormControl('', Validators.required),
-      confirm: new FormControl('', Validators.required)
+    this.user = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    account: this.fb.group({
+      email: ['', Validators.required],
+      confirm: ['', Validators.required]
     })
   });
   }
